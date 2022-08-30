0b1bc9dd3bc13d0ea77845f0801ed667fe5b2666
Anil Yadav <76562286+anil790618@users.noreply.github.com>
2022-08-30 10:05:12 +0530

    default template page in wp

---
+++ page.php
@@ -0,0 +1,13 @@
+<?php
+get_header(); 
+?>
+<div class="container">
+    <div class="row">
+        <div class="col-10 m-auto">
+            <div class="display-2"><?php  the_title()  ?></div>
+        </div>
+    </div>
+</div>
+<?php
+get_footer(); 
+?>
