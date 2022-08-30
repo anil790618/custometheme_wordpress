0ded0470187de8e8f1405ade87ceabf0242a4e9f
Anil Yadav <76562286+anil790618@users.noreply.github.com>
2022-08-30 09:52:46 +0530

    custome menu create and display

--- header.php
+++ header.php
@@ -55,13 +55,17 @@
                      <div class="limit-box">
                         <nav class="main-menu">
                            <ul class="menu-area-main">
-                              <li class="active"> <a href="index.html">Home</a> </li>
+                            <?php
+                                wp_nav_menu( array('theme_location'=>'primary-menu',
+                                                    'menu_class'=>'nav') )             
+                            ?>
+                              <!-- <li class="active"> <a href="index.html">Home</a> </li>
                               <li> <a href="#about">About</a> </li>
                               <li> <a href="#service"> Service</a> </li>
                               <li> <a href="#screenshot">Screenshot</a> </li>
                               <li> <a href="#blog">Blog</a> </li>
                               <li> <a href="#contact">Contact us</a> </li>
-                              <li class="mean-last"> <a href="#"><img src="<?php echo get_template_directory_uri(); ?>/images/search_icon.png" alt="#" /></a> </li>
+                              <li class="mean-last"> <a href="#"><img src="<?php echo get_template_directory_uri(); ?>/images/search_icon.png" alt="#" /></a> </li> -->
                            </ul>
                         </nav>
                      </div>
